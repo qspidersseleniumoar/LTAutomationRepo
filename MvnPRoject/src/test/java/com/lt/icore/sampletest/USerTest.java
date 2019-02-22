9e6ebfa553672241d4fc7d81324712556b6e87cc
deepak <deepak@gmail.com>
2019-02-22 11:19:25 +0530

    commit 8

--- MvnPRoject/src/test/java/com/lt/icore/sampletest/USerTest.java
+++ MvnPRoject/src/test/java/com/lt/icore/sampletest/USerTest.java
@@ -6,8 +6,27 @@ public class USerTest {
 
 	@Test
 	public  void createusertest() {
-		System.out.println("test");
+		System.out.println("execute createusertest");
 
 	}
+	@Test
+	public  void modifyusertest() {
+		System.out.println("execute modifyusertest");
+
+	}
+	
+	@Test
+	public  void deleteusertest() {
+		System.out.println("execute deleteusertest");
+
+	}
+	
+	
+	@Test
+	public  void searchusertest() {
+		System.out.println("execute searchusertest");
+
+	}
+
 
 }
